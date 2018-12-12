c78af4ea9fa8fcc8d6a1feb89675d343ae5ae60e
SWING1993 <songguohua10446627@gmail.com>
2018-12-12 17:45:41 +0800

    update

---
+++ src/main/java/com/swing/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.swing.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MessageController {
+
+    @RequestMapping("/message/go")
+    public String goTest() {
+        return "reach";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
