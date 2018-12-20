a423c567f38372cb9e9d691fa6846bed521aa251
SWING1993 <songguohua10446627@gmail.com>
2018-12-20 17:55:37 +0800

    全局统一处理错误

--- src/main/java/com/swing/controller/IndexController.java
+++ src/main/java/com/swing/controller/IndexController.java
@@ -1,34 +1,24 @@
 package com.swing.controller;
 
+import com.swing.utils.RestResult;
+import com.swing.utils.RestResultGenerator;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * 异常增强，以JSON的形式返回给客服端
- * 异常增强类型：NullPointerException,RunTimeException,ClassCastException,
- 　　　　　　　　 NoSuchMethodException,IOException,IndexOutOfBoundsException
- 　　　　　　　　 以及springmvc自定义异常等，如下：
- SpringMVC自定义异常对应的status code
- Exception                       HTTP Status Code
- ConversionNotSupportedException         500 (Internal Server Error)
- HttpMessageNotWritableException         500 (Internal Server Error)
- HttpMediaTypeNotSupportedException      415 (Unsupported Media Type)
- HttpMediaTypeNotAcceptableException     406 (Not Acceptable)
- HttpRequestMethodNotSupportedException  405 (Method Not Allowed)
- NoSuchRequestHandlingMethodException    404 (Not Found)
- TypeMismatchException                   400 (Bad Request)
- HttpMessageNotReadableException         400 (Bad Request)
- MissingServletRequestParameterException 400 (Bad Request)
-
- */
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
-@Controller
 @ResponseBody
+@Controller
 public class IndexController {
 
     @RequestMapping("/")
@@ -36,8 +26,69 @@ public class IndexController {
         return "Hello World!";
     }
 
-//    // 运行时异常
-//    @ExceptionHandler(RuntimeException.class)
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    public ErrorResponse run
+
+    //运行时异常
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+
+    public RestResult<String > runtimeExceptionHandler(RuntimeException runtimeException) throws Exception {
+        return RestResultGenerator.genErrorResult(runtimeException.getMessage());
+    }
+
+    //空指针异常
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestResult<String > nullPointerExceptionHandler(NullPointerException ex) throws Exception {
+        return RestResultGenerator.genErrorResult(ex.getMessage());
+    }
+
+    /*----- REQUEST ERROR -----*/
+
+    //400错误
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public RestResult<String > requestNotReadable(HttpMessageNotReadableException ex) throws Exception {
+//        System.out.println("400..requestNotReadable");
+//        ex.printStackTrace();
+//        return ErrorResponse.of("Request Not Readable", ErrorCode.BAD_REQUEST, HttpStatus.BAD_REQUEST);
+        return RestResultGenerator.genErrorResult(ex.getMessage());
+
+    }
+
+    //400错误
+    @ExceptionHandler({TypeMismatchException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public RestResult<String > requestTypeMismatch(TypeMismatchException ex) throws Exception {
+//        System.out.println("400..TypeMismatchException");
+//        ex.printStackTrace();
+//        return ErrorResponse.of("Type Mismatch Exception", ErrorCode.BAD_REQUEST, HttpStatus.BAD_REQUEST);
+//        return RestResultGenerator.genErrorResult("Type Mismatch Exception", ex.getErrorCode(), HttpStatus.BAD_REQUEST);
+        return RestResultGenerator.genErrorResult(ex.getMessage());
+    }
+
+    //400错误
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public RestResult<String > requestMissingServletRequest(MissingServletRequestParameterException ex) throws Exception {
+        ex.printStackTrace();
+        //return ErrorResponse.of("Missing Servlet Request", ErrorCode.BAD_REQUEST, HttpStatus.BAD_REQUEST);
+        return RestResultGenerator.genErrorResult(ex.getMessage());
+    }
+
+    //405错误
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public RestResult<String > request405() throws Exception {
+//        return ErrorResponse.of("Method Not Allowed", ErrorCode.METHOD_NOT_ALLOWED, HttpStatus.METHOD_NOT_ALLOWED);
+        return RestResultGenerator.genErrorResult("Method Not Allowed");
+    }
+
+    //500的异常会被这个方法捕获
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestResult<String > handleError(HttpServletRequest req, HttpServletResponse rsp, Exception e) throws Exception {
+        return RestResultGenerator.genErrorResult(e.getMessage());
+    }
+
+
 }
