ac2e74811fc91272c03568841d3b1b32257a8567
SWING1993 <songguohua10446627@gmail.com>
2019-01-18 14:56:06 +0800

    update

--- src/main/java/com/swing/controller/ExceptionController.java
+++ src/main/java/com/swing/controller/ExceptionController.java
@@ -2,6 +2,8 @@ package com.swing.controller;
 
 import com.swing.utils.RestResult;
 import com.swing.utils.RestResultGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +16,18 @@ import com.swing.utils.DingChatBot;
 @ResponseBody
 public class ExceptionController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
     //捕获异常
     @ExceptionHandler(Exception.class)
-    public RestResult<String > handleError(HttpServletRequest req, Exception ex) throws Exception {
+    public RestResult<String> handleError(HttpServletRequest req, Exception ex) throws Exception {
         Map map = new HashMap();
         map.put("url",req.getRequestURI());
         map.put("query",req.getQueryString());
         map.put("info",ex.toString());
         String errorMsg = "服务异常\n" + map.toString();
         DingChatBot.sendMsg(errorMsg);
+        logger.info(errorMsg);
         return RestResultGenerator.genErrorResult("服务器异常，请稍后再试！", 10001, map.toString());
     }
 }
